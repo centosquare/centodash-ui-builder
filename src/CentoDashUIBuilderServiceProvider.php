b7aed438108f205703390b41196c8ee091d8b285
Muhammad Waqas <mwaqasiu@gmail.com>
2023-07-01 22:54:07 +0500

    basic library setup

---
+++ src/CentoDashUIBuilderServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CentoSquare\CentoDashUIBuilder;
+
+use Illuminate\Support\ServiceProvider;
+
+class CentoDashUIBuilderServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //implementation here...
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //implementation here...
+    }
+}
